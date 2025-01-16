2007d260686fab6cd063ec821ef9f57ce6425a90
Bushrajan <darkprince67890@gmail.com>
2025-01-17 00:41:57 +0500

    update form

--- Assets/UserRecipies folder/asset/script , style/script.js
+++ Assets/UserRecipies folder/asset/script , style/script.js
@@ -1,52 +1,59 @@
-// ---------------------------------------------------------------------------------------------
 document.addEventListener('DOMContentLoaded', function () {
-  const text = document.getElementById('text')
-  const inputField = document.getElementById('inputFieldText')
-  const filePreview = document.getElementById('filePreview')
-  const changeButton = document.getElementById('changeButton')
+  const text = document.getElementById('text');
+  const inputField = document.getElementById('image');
+  const filePreview = document.getElementById('filePreview');
+  const changeButton = document.getElementById('changeButton');
 
   text.addEventListener('click', function () {
-    inputField.click()
-  })
+    inputField.click();
+  });
 
   inputField.addEventListener('change', function () {
-    const file = inputField.files[0]
-    const fileURL = URL.createObjectURL(file)
-    let fileElement
+    const file = inputField.files[0];
+    const reader = new FileReader();
 
-    if (file.type.startsWith('image/')) {
-      fileElement = document.createElement('img')
-      fileElement.src = fileURL
-      fileElement.style.maxWidth = '100%'
-      fileElement.style.height = 'auto'
-    } else if (file.type.startsWith('video/')) {
-      fileElement = document.createElement('video')
-      fileElement.src = fileURL
-      fileElement.controls = true
-      fileElement.style.maxWidth = '100%'
-      fileElement.style.height = 'auto'
-    } else {
-      fileElement = document.createElement('p')
-      fileElement.textContent = 'Unsupported file type.'
-    }
+    reader.onload = function (e) {
+      const fileURL = e.target.result;
+      let fileElement;
 
-    // Clear the previous content and add the new file element
-    filePreview.innerHTML = ''
-    filePreview.appendChild(fileElement)
+      if (file.type.startsWith('image/')) {
+        fileElement = document.createElement('img');
+        fileElement.src = fileURL;
+        fileElement.style.maxWidth = '100%';
+        fileElement.style.height = 'auto';
+      } else if (file.type.startsWith('video/')) {
+        fileElement = document.createElement('video');
+        fileElement.src = fileURL;
+        fileElement.controls = true;
+        fileElement.style.maxWidth = '100%';
+        fileElement.style.height = 'auto';
+      } else {
+        fileElement = document.createElement('p');
+        fileElement.textContent = 'Unsupported file type.';
+      }
 
-    // Hide the "Add Image / Video" text
-    text.style.display = 'none'
+      filePreview.innerHTML = '';
+      filePreview.appendChild(fileElement);
+      text.style.display = 'none';
+      changeButton.style.display = 'inline-block';
 
-    // Show the "Change" button
-    changeButton.style.display = 'inline-block'
-  })
+      // Save the base64 encoded image to localStorage
+      const formData = JSON.parse(localStorage.getItem('formData')) || {};
+      formData.file = fileURL; // Use base64 encoded image
+      localStorage.setItem('formData', JSON.stringify(formData));
+    };
+
+    reader.readAsDataURL(file);
+  });
 
   changeButton.addEventListener('click', function () {
-    inputField.click()
-  })
-})
+    inputField.click();
+  });
+});
 
-////////////////////////   recipietitle
+
+
+// -------------------  recipie title -------------------------------   
 document.addEventListener('DOMContentLoaded', function () {
   const input = document.getElementById('recipeTitle')
 
@@ -65,7 +72,8 @@ document.addEventListener('DOMContentLoaded', function () {
   })
 })
 
-// ---------------------------------------------------------------------------------------------
+
+// -------------------  recipie checkbox -------------------------------   
 document.addEventListener('click', function () {
   const checkboxes = document.querySelectorAll('.form-check-input')
 
@@ -125,37 +133,29 @@ document.addEventListener('DOMContentLoaded', function () {
     ingredientField.value = ingredientText
     ingredientField.readOnly = true
 
-    // Create the edit button using SVG
-    const editBtnIngredient = document.createElement('span')
+    // Create the edit button for ingredient using Font Awesome
+    const editBtnIngredient = document.createElement('span');
     editBtnIngredient.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <g fill="none" stroke="#635C5F" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-          <path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1"/>
-          <path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/>
-        </g>
-      </svg>
-    `
-    editBtnIngredient.classList.add('edit-btn-svg-container')
+  <i class="fas fa-edit" style="width: 24px; height: 24px;"></i>`;
+    editBtnIngredient.classList.add('edit-btn-svg-container');
     editBtnIngredient.addEventListener('click', function () {
       if (ingredientField.readOnly) {
-        ingredientField.readOnly = false
-        ingredientField.focus()
+        ingredientField.readOnly = false;
+        ingredientField.focus();
       } else {
-        ingredientField.readOnly = true
+        ingredientField.readOnly = true;
       }
-    })
+    });
 
-    // Create the delete button using SVG
-    const deleteBtnIngredient = document.createElement('span')
+    // Create the delete button for ingredient using Font Awesome
+    const deleteBtnIngredient = document.createElement('span');
     deleteBtnIngredient.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 56 56">
-        <path fill="#635C5F" d="M46.035 49.574c4.899 0 7.36-2.414 7.36-7.265V13.69c0-4.851-2.461-7.265-7.36-7.265H25.668c-2.742 0-5.11.703-7.031 2.742L4.832 23.512c-1.523 1.57-2.226 2.976-2.226 4.453c0 1.453.68 2.883 2.226 4.453L18.66 46.691c1.946 2.016 4.29 2.86 7.032 2.86Zm-5.46-11.203c-.563 0-1.055-.21-1.454-.586l-6.844-6.89l-6.867 6.89c-.398.375-.89.586-1.453.586c-1.148 0-2.11-.937-2.11-2.086c0-.539.235-1.054.634-1.476l6.82-6.844l-6.82-6.82c-.399-.422-.633-.938-.633-1.477c0-1.172.96-2.133 2.11-2.133c.538 0 1.054.211 1.476.633l6.843 6.844l6.82-6.844c.423-.422.938-.633 1.477-.633c1.172 0 2.11.961 2.11 2.133c0 .54-.211 1.055-.633 1.477l-6.82 6.82l6.82 6.844c.422.422.633.937.633 1.476c0 1.149-.961 2.086-2.11 2.086"/>
-      </svg>
-    `
-    deleteBtnIngredient.classList.add('delimg', 'ms-4', 'me-3')
+  <i class="fas fa-trash-alt" style="width: 24px; height: 24px;"></i>`;
+    deleteBtnIngredient.classList.add('delimg', 'ms-4', 'me-3');
     deleteBtnIngredient.addEventListener('click', function () {
-      ingredientList.removeChild(container)
-    })
+      ingredientList.removeChild(container);
+    });
+
 
     // Append elements to the ingredient container
     ingredientContainer.appendChild(ingredientField)
@@ -181,37 +181,30 @@ document.addEventListener('DOMContentLoaded', function () {
     quantityField.value = 'Quantity : 2 / 3'
     quantityField.readOnly = true
 
-    // Create the edit button for quantity
-    const editBtnQuantity = document.createElement('span')
+    // Create the edit button for quantity using Font Awesome
+    const editBtnQuantity = document.createElement('span');
     editBtnQuantity.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <g fill="none" stroke="#635C5F" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-          <path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1"/>
-          <path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/>
-        </g>
-      </svg>
-    `
-    editBtnQuantity.classList.add('editimg')
+  <i class="fas fa-edit" ></i>
+`;
+    editBtnQuantity.classList.add('editimg');
     editBtnQuantity.addEventListener('click', function () {
       if (quantityField.readOnly) {
-        quantityField.readOnly = false
-        quantityField.focus()
+        quantityField.readOnly = false;
+        quantityField.focus();
       } else {
-        quantityField.readOnly = true
+        quantityField.readOnly = true;
       }
-    })
+    });
 
-    // Create the delete button for quantity
-    const deleteBtnQuantity = document.createElement('span')
+    // Create the delete button for quantity using Font Awesome
+    const deleteBtnQuantity = document.createElement('span');
     deleteBtnQuantity.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 56 56">
-        <path fill="#635C5F" d="M46.035 49.574c4.899 0 7.36-2.414 7.36-7.265V13.69c0-4.851-2.461-7.265-7.36-7.265H25.668c-2.742 0-5.11.703-7.031 2.742L4.832 23.512c-1.523 1.57-2.226 2.976-2.226 4.453c0 1.453.68 2.883 2.226 4.453L18.66 46.691c1.946 2.016 4.29 2.86 7.032 2.86Zm-5.46-11.203c-.563 0-1.055-.21-1.454-.586l-6.844-6.89l-6.867 6.89c-.398.375-.89.586-1.453.586c-1.148 0-2.11-.937-2.11-2.086c0-.539.235-1.054.634-1.476l6.82-6.844l-6.82-6.82c-.399-.422-.633-.938-.633-1.477c0-1.172.96-2.133 2.11-2.133c.538 0 1.054.211 1.476.633l6.843 6.844l6.82-6.844c.423-.422.938-.633 1.477-.633c1.172 0 2.11.961 2.11 2.133c0 .54-.211 1.055-.633 1.477l-6.82 6.82l6.82 6.844c.422.422.633.937.633 1.476c0 1.149-.961 2.086-2.11 2.086"/>
-      </svg>
-    `
-    deleteBtnQuantity.classList.add('delimg', 'ms-4', 'me-3')
+  <i class="fas fa-trash-alt" ></i>
+`;
+    deleteBtnQuantity.classList.add('delimg', 'ms-4', 'me-3');
     deleteBtnQuantity.addEventListener('click', function () {
-      ingredientList.removeChild(container)
-    })
+      ingredientList.removeChild(container);
+    });
 
     // Append elements to the quantity container
     quantityContainer.appendChild(quantityField)
@@ -269,11 +262,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   })
 
-  function addStep (stepText) {
+  function addStep(stepText) {
     const stepItem = document.createElement('div')
     stepItem.classList.add(
       'input-group',
-      'mt-lg-1',
+      // 'mt-lg-2',
       'p-2',
       'mb-2',
       'Ingredient',
@@ -286,40 +279,34 @@ document.addEventListener('DOMContentLoaded', function () {
     stepField.value = stepText
     stepField.readOnly = true
 
-    // Create the edit button using SVG
-    const editBtn = document.createElement('span')
+    // Create the edit button using Font Awesome
+    const editBtn = document.createElement('span');
     editBtn.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
-        <g fill="none" stroke="#635C5F" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
-          <path d="M7 7H6a2 2 0 0 0-2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2-2v-1"/>
-          <path d="M20.385 6.585a2.1 2.1 0 0 0-2.97-2.97L9 12v3h3zM16 5l3 3"/>
-        </g>
-      </svg>
-    `
-    editBtn.classList.add('editimg')
+  <i class="fas fa-edit" ></i>
+`;
+    editBtn.classList.add('editimg');
     editBtn.addEventListener('click', function () {
       if (stepField.readOnly) {
-        stepField.readOnly = false
-        stepField.focus()
+        stepField.readOnly = false;
+        stepField.focus();
       } else {
-        stepField.readOnly = true
+        stepField.readOnly = true;
       }
-    })
+    });
 
-    // Create the delete button using SVG
-    const deleteBtn = document.createElement('span')
+    // Create the delete button using Font Awesome
+    const deleteBtn = document.createElement('span');
     deleteBtn.innerHTML = `
-      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 56 56">
-        <path fill="#635C5F" d="M46.035 49.574c4.899 0 7.36-2.414 7.36-7.265V13.69c0-4.851-2.461-7.265-7.36-7.265H25.668c-2.742 0-5.11.703-7.031 2.742L4.832 23.512c-1.523 1.57-2.226 2.976-2.226 4.453c0 1.453.68 2.883 2.226 4.453L18.66 46.691c1.946 2.016 4.29 2.86 7.032 2.86Zm-5.46-11.203c-.563 0-1.055-.21-1.454-.586l-6.844-6.89l-6.867 6.89c-.398.375-.89.586-1.453.586c-1.148 0-2.11-.937-2.11-2.086c0-.539.235-1.054.634-1.476l6.82-6.844l-6.82-6.82c-.399-.422-.633-.938-.633-1.477c0-1.172.96-2.133 2.11-2.133c.538 0 1.054.211 1.476.633l6.843 6.844l6.82-6.844c.423-.422.938-.633 1.477-.633c1.172 0 2.11.961 2.11 2.133c0 .54-.211 1.055-.633 1.477l-6.82 6.82l6.82 6.844c.422.422.633.937.633 1.476c0 1.149-.961 2.086-2.11 2.086"/>
-      </svg>
-    `
-    deleteBtn.classList.add('delimg', 'ms-4', 'me-3')
+  <i class="fas fa-trash-alt" ></i>
+`;
+    deleteBtn.classList.add('delimg', 'ms-4', 'me-3');
     deleteBtn.addEventListener('click', function () {
-      stepsList.removeChild(stepItem)
-      stepCount--
-      totalCharacterCount -= stepField.value.length
-      reorderSteps()
-    })
+      stepsList.removeChild(stepItem);
+      stepCount--;
+      totalCharacterCount -= stepField.value.length;
+      reorderSteps();
+    });
+
 
     stepItem.appendChild(stepField)
     stepItem.appendChild(editBtn)
@@ -332,7 +319,7 @@ document.addEventListener('DOMContentLoaded', function () {
     reorderSteps()
   }
 
-  function reorderSteps () {
+  function reorderSteps() {
     const stepInputs = stepsList.querySelectorAll('.Ingredient input')
     stepInputs.forEach((input, index) => {
       input.placeholder = `Steps ... `
@@ -340,7 +327,7 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 })
 
-// ---------------------------------------------------------------------------------------------
+// --------------------------------- additional Btn-------------------------------------
 
 document.addEventListener('DOMContentLoaded', function () {
   const additionalBtn = document.getElementById('additionalBtn')
@@ -377,22 +364,18 @@ document.addEventListener('DOMContentLoaded', function () {
 
 // ---------------------------------------------------------------------------------------
 
-document.getElementById('exampleInput').addEventListener('blur', function () {
-  this.classList.add('form-control-blur')
-})
-
 document.addEventListener('DOMContentLoaded', function () {
   // yahn form clear ho jai ga agr koi is page main wapis ata hy tooo
   const form = document.querySelector('form')
-  form.reset()
+  form.clear();
 
   document.getElementById('submitBtn').addEventListener('click', submitbtn)
   document.querySelector('.btn-close').addEventListener('click', closePopup)
 })
 
-function submitbtn () {
+function submitbtn() {
   console.log('Submit button clicked') // Log to check if the function is called
-  const inputField = document.getElementById('inputFieldText')
+  const inputField = document.getElementById('image')
   const recipeTitle = document.getElementById('recipeTitle')
   const floatingTextarea2 = document.getElementById('floatingTextarea2')
   const servingInput = document.querySelector('input[placeholder="4 persons "]')
@@ -422,32 +405,41 @@ function submitbtn () {
     return
   }
 
-  ////////////// console per form fill ho jai ga yahn
-  console.log('Form filled values:')
-  console.log('Image/Video:', inputField.files[0].name)
-  console.log('Recipe Title:', recipeTitle.value)
-  console.log('Ingredients:')
-  ingredientInputs.forEach((ingredientInput, index) => {
-    console.log(`Ingredient ${index + 1}:`, ingredientInput.value)
-  })
-  console.log('Cooking Steps:')
-  stepInputs.forEach((stepInput, index) => {
-    console.log(`Step ${index + 1}:`, stepInput.value)
-  })
-  console.log('Description:', floatingTextarea2.value)
-  console.log('Servings:', servingInput.value)
-  console.log('Preparation Time:', preparationInput.value)
-  console.log('Categories:')
-  checkboxes.forEach((checkbox, index) => {
-    if (checkbox.checked) {
-      console.log(
-        `Category ${index + 1}:`,
-        checkbox.nextElementSibling.innerText
-      )
-    }
-  })
 
-  ///////////// Show the popup
+  //--------------------------------local storagemain save ho raha hy -----------------------------
+  // Validate all required fields
+  if (
+    inputField.files.length === 0 ||
+    recipeTitle.value.trim() === '' ||
+    floatingTextarea2.value.trim() === '' ||
+    servingInput.value.trim() === '' ||
+    preparationInput.value.trim() === '' ||
+    !Array.from(checkboxes).some(checkbox => checkbox.checked) ||
+    stepsList.children.length === 0
+  ) {
+    alert('Please fill the form completely before submitting.');
+    console.log('Form validation failed');
+    return;
+  }
+
+  const formData = {
+    file: JSON.parse(localStorage.getItem('formData'))?.file || 'No file selected', // Retrieve from localStorage
+    recipeTitle: recipeTitle.value,
+    description: floatingTextarea2.value,
+    servings: servingInput.value,
+    preparationTime: preparationInput.value,
+    categories: Array.from(checkboxes).filter(checkbox => checkbox.checked).map(checkbox => checkbox.nextElementSibling.innerText)
+  };
+
+  
+  let userCollection = JSON.parse(localStorage.getItem('userCollection')) || [];
+  userCollection.push(formData);
+  localStorage.setItem('userCollection', JSON.stringify(userCollection));
+  console.log('Form data saved to localStorage:', formData);
+
+
+
+  //--------------- Show the popup --------------------
   const popupcontainer = document.getElementById('backgroundpopupbox')
   popupcontainer.style.display = 'block'
   console.log('Popup shown')
@@ -456,14 +448,16 @@ function submitbtn () {
   setTimeout(function () {
     tickImage.style.display = 'block'
   }, 3)
+  
 }
 
-function closePopup () {
-  console.log('Close button clicked')
+
+
+function closePopup() {
   const popupcontainer = document.getElementById('backgroundpopupbox')
   popupcontainer.style.display = 'none'
   window.location.href = "./recipiemain.html"
-  console.log('Popup closed and redirected to recipiemain.html')
+
 }
 
 
