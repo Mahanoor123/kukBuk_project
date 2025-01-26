6477894579756d0af812328e01b5186a97c4ef3d
kirankashif <kirankashif933@gmail.com>
2025-01-26 23:43:40 +0500

    Blogs files added

--- Assets/blog-page/blogs-assets/index.js
+++ Assets/blog-page/blogs-assets/index.js
@@ -3,15 +3,30 @@ document.querySelector('.hamburger').addEventListener('click', () => {
 });
 
 
-let popup = document.getElementById("pop-up")
+    function openPopup() {
+        document.getElementById('pop-up').style.display = 'block';
+      }
+      
+      function closePopup() {
+        document.getElementById('pop-up').style.display = 'none';
+      }
+
+      function showtext()
+{var text="The flavors of mediterrenean are cooked upto 30 minutes in an instant pot.for a quick and filling. Savoury inspired for weeknight. The flavors of mediterrenean are cooked upto 30 minutes in an instant pot.for a quick and filling. Savoury inspired for weeknight.The flavors of mediterrenean are cooked upto 30 minutes in an instant pot.for a quick and filling.The flavors of mediterrenean are cooked upto 30 minutes in an instant pot.for a quick and filling. Savoury inspired for weeknight. The flavors of mediterrenean are cooked upto 30 minutes in an instant pot.for a quick and filling. Savoury inspired for weeknight.The flavors of mediterrenean are cooked upto 30 minutes in an instant pot.for a quick and filling.";
+document.getElementById("textarea").innerHTML=text;
+}
 
 
-function openPopup() {
-popup.classList.add("open-popup");
+function myFunction(){
+    document.getElementById('more-card').style.display = 'block';
+    document.getElementById('more-card1').style.display = 'block';
+    document.getElementById('more-card2').style.display = 'block';
+    document.getElementById('more-card3').style.display = 'block';
+
+
 
 }
 
-function closePopup() {
-    popup.classList.remove("open-popup");
-    
-    }
+
+
+
