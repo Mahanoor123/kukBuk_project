abce09755730a64f7f4b88d6b9c889cdf228a545
Bushrajan <darkprince67890@gmail.com>
2025-02-25 18:29:56 +0500

    editing funtiing in userrecipe

--- public-src/Assets/User-recipes/js/userRecipe.js
+++ public-src/Assets/User-recipes/js/userRecipe.js
@@ -1,4 +1,22 @@
-import { db, collection, doc, getDocs, query, orderBy, deleteDoc, updateDoc, setDoc, onSnapshot, } from '/firebase/firebase-config.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
+import { getFirestore, where } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCMeJCTXbdJorjOdi-5woYwoG6rb7ddDDQ",
+  authDomain: "kukbuk-project-45362.firebaseapp.com",
+  projectId: "kukbuk-project-45362",
+  storageBucket: "kukbuk-project-45362.firebasestorage.app",
+  messagingSenderId: "695377977253",
+  appId: "1:695377977253:web:e4bf823163001937c6924c",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+import { collection, doc, getDocs, getDoc, query, orderBy, deleteDoc, updateDoc, setDoc, onSnapshot, } from '/firebase/firebase-config.js';
+
 
 /***********************************************/
 /*********** Overall function working **********/
@@ -13,108 +31,96 @@ document?.addEventListener('DOMContentLoaded', function (event) {
   /**************************************************/
   /*********** Fetch and display recipes ************/
   /**************************************************/
-  const fetchAndDisplayCards = async () => {
-    try {
-      const querySnapshot = await getDocs(query(collection(db, "userrecipie"), orderBy("timestamp", "desc")));
-      userCollection = [];
+  onAuthStateChanged(auth, async (user) => {
+    if (user) {
+      console.log("User is signed in:", user);
 
-      querySnapshot.forEach(doc => {
-        const data = doc.data();
-        userCollection.push({ id: doc.id, ...data });
-      });
+      try {
+        console.log("User UID:", user.uid); // Log the user UID
 
-      displayCards(userCollection, "userCollection");
+        const querySnapshot = await getDocs(query(
+          collection(db, "userrecipie"),
+          where("userId", "==", user.uid),
+          orderBy("timestamp", "desc")
+        ));
+
+
+        let userCollection = [];
+
+        querySnapshot.forEach(doc => {
+          const data = doc.data();
+          console.log("Document Data:", data); // Log the document data
+          userCollection.push({ id: doc.id, ...data });
+        });
+
+        if (userCollection.length == 0) {
+          console.log("userCollection = 0 , No-recipies.");
+        }
+
+        else {
+          console.log("User Collection:", userCollection); // Log the user collection
+          displayCards(userCollection, "userCollection");
+        }
+
+      }
+      catch (error) {
+        console.error("Error fetching user recipes from Firestore:", error);
+      }
     }
-    catch (error) {
-      console.error("Error fetching user recipes from Firestore: ", error);
+    else {
+      console.log("No user is signed in.");
     }
-
-  };
-
-
-  /**************************************************/
-  /*********** Fetch and display favorite recipes ************/
-  /**************************************************/
-  const fetchFavoriteCards = async () => {
-    try {
-      const querySnapshot = await getDocs(collection(db, "favouriterecipie-card"));
-      favouriteCollection = [];
-
-      querySnapshot.forEach(doc => {
-        const data = doc.data();
-        favouriteCollection.push({ id: doc.id, ...data });
-      });
-
-      displayCards(favouriteCollection, "favouriteusercollection");
-    } catch (error) {
-      console.error("Error fetching favorite recipes from Firestore: ", error);
-    }
-  };
-
-
-
-  /******************************************/
-  /*********** Display recipes **************/
-  /******************************************/
-  document.addEventListener("DOMContentLoaded", (event) => {
-    setUpRealTimeListeners();
   });
 
 
-  const displayCards = (collection, containerId, userId) => {
-  const container = document.getElementById(containerId);
-  if (!container) {
-    return;
-  }
 
-  container.innerHTML = '';
-  collection.forEach((recipe, index) => {
-    if (recipe.userId === userId) { // Filter by user ID
+
+  const displayCards = (collection, containerId) => {
+    const container = document.getElementById(containerId);
+    if (!container) {
+      return;
+    }
+
+    container.innerHTML = '';
+    collection.forEach((recipe, index) => {
       const card = document.createElement("div");
       card.setAttribute("class", "col");
       card.innerHTML = `
-      
-    <div class="recipe card recipie-card">
-        <div class="recipe-img-div">
+        <div class="recipe card recipie-card">
+          <div class="recipe-img-div">
             <img src="${recipe.imageURL}" class="recipe_img" width="200px" height="200px">
-        </div>
-        <div class="recipe_content">
+          </div>
+          <div class="recipe_content">
             <h4 class="h4">${recipe.recipeTitle}</h4>
             <div class="d-flex text-center mt-4 flex-column align-items-center justify-content-between mt-lg-4 mb-4 ">
-                <div class="d-flex text-center flex-column align-items-center justify-content-between mt-lg-4 mb-4 ">
-                    <div class="recipie-span-div d-flex align-items-center justify-content-center">
-                        <div> <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 48 48">
-                                <defs>
-                                    <mask id="ipSTag0">
-                                        <g fill="none" stroke-linejoin="round" stroke-width="4">
-                                            <path fill="#fff" stroke="#fff"
-                                                d="M8 44V6a2 2 0 0 1 2-2h28a2 2 0 0 1 2 2v38l-16-8.273z" />
-                                            <path stroke="#000" stroke-linecap="round" d="M16 18h16" />
-                                        </g>
-                                    </mask>
-                                </defs>
-                                <path fill="#e86209" d="M0 0h48v48H0z" mask="url(#ipSTag0)" />
-                            </svg> Category : &nbsp; </div>&nbsp;
-                        <span>
-
-                            <small><span class="recipie-span">${recipe.category.join(", ")}</span></small>
-                        </span>
-
-                    </div>
-                    <div class="recipie-span-div d-flex align-items-center justify-content-center">
-
-                        <div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 2048 2048">
-                                <path fill="#e86209"
-                                    d="M1280 64q0 179 66 330t190 278t190 278t66 330q0 106-27 204t-78 183t-120 156t-155 120t-184 77t-204 28t-204-27t-183-78t-156-120t-120-155t-77-184t-28-204q0-84 18-165t52-155t84-141t113-121q7 38 19 78t28 80t38 76t46 67q20 25 52 25q27 0 45-19t19-46q0-11-3-20t-10-18q-28-41-49-81t-37-82t-23-87t-8-95q0-119 45-224t124-183T992 46t224-46h64zm-256 1856q133 0 249-50t204-137t137-203t50-250q0-151-56-281t-162-236q-130-131-204-289t-88-342q-83 11-153 50t-123 99t-81 135t-29 160q0 78 23 141t68 126q19 26 29 54t11 62q0 40-15 75t-42 61t-61 42t-75 15q-46 0-81-17t-62-46t-48-65t-40-72q-46 73-68 157t-23 171q0 133 50 249t137 204t203 137t250 50" />
-                            </svg>
-                            Calories : &nbsp;
-                        </div>&nbsp;
-                        <span>
-
-                            <small><span class="recipie-span">${recipe.calories}</span></small>
-                        </span>
-                    </div>
+              <div class="d-flex text-center flex-column align-items-center justify-content-between mt-lg-4 mb-4 ">
+                <div class="recipie-span-div d-flex align-items-center justify-content-center">
+                  <div> 
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 48 48">
+                      <defs>
+                        <mask id="ipSTag0">
+                          <g fill="none" stroke-linejoin="round" stroke-width="4">
+                            <path fill="#fff" stroke="#fff" d="M8 44V6a2 2 0 0 1 2-2h28a2 2 0 0 1 2 2v38l-16-8.273z"/>
+                            <path stroke="#000" stroke-linecap="round" d="M16 18h16"/>
+                          </g>
+                        </mask>
+                      </defs>
+                      <path fill="#e86209" d="M0 0h48v48H0z" mask="url(#ipSTag0)"/>
+                    </svg> Category : &nbsp;
+                  </div>&nbsp;
+                  <span>
+                    <small><span class="recipie-span">${recipe.category.join(", ")}</span></small>
+                  </span>
+                </div>
+                <div class="recipie-span-div d-flex align-items-center justify-content-center">
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 2048 2048">
+                      <path fill="#e86209" d="M1280 64q0 179 66 330t190 278t190 278t66 330q0 106-27 204t-78 183t-120 156t-155 120t-184 77t-204 28t-204-27t-183-78t-156-120t-120-155t-77-184t-28-204q0-84 18-165t52-155t84-141t113-121q7 38 19 78t28 80t38 76t46 67q20 25 52 25q27 0 45-19t19-46q0-11-3-20t-10-18q-28-41-49-81t-37-82t-23-87t-8-95q0-119 45-224t124-183T992 46t224-46h64zm-256 1856q133 0 249-50t204-137t137-203t50-250q0-151-56-281t-162-236q-130-131-204-289t-88-342q-83 11-153 50t-123 99t-81 135t-29 160q0 78 23 141t68 126q19 26 29 54t11 62q0 40-15 75t-42 61t-61 42t-75 15q-46 0-81-17t-62-46t-48-65t-40-72q-46 73-68 157t-23 171q0 133 50 249t137 204t203 137t250 50"/>
+                  </div>&nbsp;
+                  <span>
+                    <small><span class="recipie-span">${recipe.calories}</span></small>
+                  </span>
+                </div>
                     <div class="recipie-span-div d-flex align-items-center justify-content-center">
                         <div>
                             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14">
@@ -167,6 +173,16 @@ document?.addEventListener('DOMContentLoaded', function (event) {
     });
 
   };
+
+
+
+
+  window.document?.addEventListener("DOMContentLoaded", (event) => {
+    event.preventDefault()
+    setUpRealTimeListeners();
+  });
+
+
 
   /***********************************************/
   /*********** Delete Card Function **************/
@@ -402,6 +418,5 @@ document?.addEventListener('DOMContentLoaded', function (event) {
     favouriteUserCollection.appendChild(card);
   };
 
-  fetchAndDisplayCards();
 });
 
